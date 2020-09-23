4eeec2fd7646d2247d06089fb982e4b7c09ed958
Kahren1 <kahren.mkrtchyan@gmail.com>
2020-09-23 16:34:40 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //to start Selenium scrip we need:
         //set up webdriver (browser driver)
         WebDriverManager.chromedriver().setup();
@@ -14,6 +14,19 @@ public class BasicNavigation {
         WebDriver driver = new ChromeDriver();
         //in selenium, everything start from WebDriver interface
         driver.get("http://google.com"); //to open a website
+        Thread.sleep(3000);//wait for demo purposes
+
+        String title = driver.getTitle();//returns page title
+        String expectedTitle="Google";
+        System.out.println("Page title is "+title);
+        if (expectedTitle.equals(title)){
+            System.out.println("test passed");
+        }else{
+            System.out.println("test failed");
+        }
+
+        //browser can't close itself
+        driver.close();
 
 
     }
